3eb5c85b6ea2dd37ef151a2ffc90cd9f9ae2c11f
Ayush Addhyan <ayushaddhyan@Ayushs-MacBook-Air.local>
2022-11-28 13:41:05 +0530

    Add perceptron simulation

---
+++ Intro.js
@@ -0,0 +1,15 @@
+// We'll be creating our own neural network using javascript that can be integrated with the frontend in the browser itself in this repository 
+// In human brain, there're entities called neurons, consisting of dendrites and axon. Human brain gets input from the surrounding and processes it and creates an output, here's in this repo we'll be simulating same scenario like that happens in human brain
+
+
+/*
+Input0 - - \
+            \    Processor -----> Output 
+            /
+Input1 - - /
+
+*/
+
+
+// It has one neuron the processor neuron, two inputs and a ouput. 
+
